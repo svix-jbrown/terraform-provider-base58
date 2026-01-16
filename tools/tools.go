cd661257ce6f2bfd40a14fe5ca8287b0daaa1208
James Brown <jbrown@svix.com>
2026-01-16 15:57:25 -0800

    fix generate

--- tools/tools.go
+++ tools/tools.go
@@ -19,4 +19,4 @@ import (
 //go:generate terraform fmt -recursive ../examples/
 
 // Generate documentation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name svix-jbrown/base58
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name base58
